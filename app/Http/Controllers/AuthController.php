7ab3cd524bbe75feb165b86934ed589093aaaf70
Misael Vargas <misaelomar.1590@hotmail.com>
2023-02-17 14:36:01 -0600

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+
+// use Furdarius\OIDConnect\Exception\AuthenticationException;
+// use Furdarius\OIDConnect\Exception\TokenStorageException;
+// use Furdarius\OIDConnect\TokenRefresher;
+// use Furdarius\OIDConnect\TokenStorage;
+// use Illuminate\Http\JsonResponse;
+// use Illuminate\Http\Request;
+// use Illuminate\Routing\Controller as BaseController;
+// use Lcobucci\JWT\Parser;
+// use Symfony\Component\HttpFoundation\RedirectResponse;
+
+class AuthController extends Controller
+{
+    /**
+     *
+     * @return RedirectResponse
+     */
+    public function redirect()
+    {
+        return Socialite::driver('authentik')->stateless()->redirect();
+    }
+
+    /**
+     * @param Request                            $request
+     * @param \Furdarius\OIDConnect\TokenStorage $storage
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function callback(Request $request, TokenStorage $storage)
+    {
+        // // TODO: handle CORS more elegant way
+        // if ($request->getMethod() === 'OPTIONS') {
+        //     return $this->responseJson([])
+        //         ->header('Access-Control-Allow-Origin', '*')
+        //         ->header('Access-Control-Allow-Methods', strtoupper($request->headers->get('Access-Control-Request-Method')))
+        //         ->header('Access-Control-Allow-Headers', $request->headers->get('Access-Control-Request-Headers'));
+        // }
+
+        /** @var \Laravel\Socialite\Two\User $user */
+        $user = Socialite::driver('authentik')->stateless()->user();
+
+        // if (!$storage->saveRefresh($user['sub'], $user['iss'], $user->refreshToken)) {
+        //     throw new TokenStorageException("Failed to save refresh token");
+        // }
+
+        return $this->responseJson([
+            'name' => $user->getName(),
+            'email' => $user->getEmail(),
+            'token' => $user->token,
+        ]);
+    }
+
+    /**
+     * @param array|\JsonSerializable $data
+     * @param int                     $status
+     * @param array                   $headers
+     *
+     * @return JsonResponse
+     */
+    protected function responseJson($data, int $status = 200, array $headers = []): JsonResponse
+    {
+        return response()->json($data, $status, $headers)
+            ->setEncodingOptions(JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE)
+            ->header('Access-Control-Allow-Origin', '*');
+    }
+
+    /**
+     * @param Request        $request
+     * @param TokenRefresher $refresher
+     * @param Parser         $parser
+     *
+     * @return AuthenticationException|JsonResponse
+     */
+    public function refresh(Request $request, TokenRefresher $refresher, Parser $parser)
+    {
+        $data = $request->json()->all();
+
+        if (!isset($data['token'])) {
+            return new AuthenticationException("Failed to get JWT token from input");
+        }
+
+        $jwt = $data['token'];
+        /**
+         * We cant get claims from Token interface, so call claims method implicitly
+         * link: https://github.com/lcobucci/jwt/pull/186
+         *
+         * @var $token \Lcobucci\JWT\Token\Plain
+         */
+        $token = $parser->parse($jwt);
+
+        $claims = $token->claims();
+
+        $sub = $claims->get('sub');
+        $iss = $claims->get('iss');
+
+        $refreshedIDToken = $refresher->refreshIDToken($sub, $iss);
+
+        return $this->responseJson([
+            'token' => $refreshedIDToken,
+        ]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/auth/redirect', [AuthController::class, 'redirect']);
+Route::get('/auth/callback', [AuthController::class, 'callback']);
+Route::options('/auth/callback', [AuthController::class, 'callback']);
+Route::post('/auth/refresh', [AuthController::class, 'refresh']);
